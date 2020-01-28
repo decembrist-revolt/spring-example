23487aa823f3446e4c05fadf7112abf8a2ec16d3
nevor.nevor <decembrist.org@gmail.com>
2020-01-28 17:39:27 +0300

    init commit

---
+++ src/main/java/org/decemrist/WildcardApplication.java
@@ -0,0 +1,13 @@
+package org.decemrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/decemrist/WildcardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.decemrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
