3d79945d4a91cc8dc64262a4978767914b244309
nevor.nevor <decembrist.org@gmail.com>
2020-02-22 06:30:07 +0300

    convert entity

---
+++ src/main/kotlin/org/decembrist/domain/User.kt
@@ -0,0 +1,18 @@
+package org.decembrist.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.SequenceGenerator
+
+@Entity
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_generator")
+    @SequenceGenerator(name = "user_id_generator", sequenceName = "USER_SEQ_ID", allocationSize = 1)
+    val id: Long? = null
+    var email: String? = null
+    var username: String? = null
+    var password: String? = null
+}
